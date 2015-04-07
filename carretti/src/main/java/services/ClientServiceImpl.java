82cd7b94ecfeca788d23e5026cf336faaea38482
hellbreak <antonio.dimariano@gmail.com>
2015-04-07 12:58:11 +0200

    inserimento dati dummy

--- carretti/src/main/java/services/ClientServiceImpl.java
+++ carretti/src/main/java/services/ClientServiceImpl.java
@@ -2,7 +2,6 @@ package services;
 
 import java.util.Hashtable;
 
-import carretti.Session;
 
 public class ClientServiceImpl implements ClientService{
 
@@ -10,8 +9,7 @@ public class ClientServiceImpl implements ClientService{
 	Hashtable<String, String> usersData = new Hashtable<String,String>();
 
 	public ClientServiceImpl() {
-		//usersData.put("user1","f543c2ccfa550171f5816c9fdc310165c7e1cc10");
-		//System.out.println("Key 'user1' has value = " + usersData.get("user1"));
+		populateDummyData();
 	}
 	
 	@Override
